0b6277921b21eea22d245dd84d5e8bb53b394b5c
Daniel Larsson <DanielLarsson@Daniels-MacBook-Pro.local>
2017-09-12 19:21:36 +0200

    Finished the swoosh tutorial

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    @IBOutlet weak var nextButton: UIButton!
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        player = Player()
     }
     
     @IBAction func nextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-
+    
+    @IBAction func mensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func womensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+        
+    }
+    
+    @IBAction func coedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func  selectLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+            skillVC.player = player
+        }
+    }
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Daniel Larsson on 2017-09-12.
+//  Copyright © 2017 Daniel Larsson. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
